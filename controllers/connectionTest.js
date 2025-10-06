02e8bd4bde304f6081991519a3fd2941f32d2c51
kappasan21 <134040762+kappasan21@users.noreply.github.com>
2025-10-06 11:54:02 +0900

    Update connectionTest.js
    Just added comments

--- controllers/connectionTest.js
+++ controllers/connectionTest.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
-
+// Connection test with THIS server
 async function connectionTest (req, res) {
   res.send("Reached WehaterAPI Server", PORT);
 };
